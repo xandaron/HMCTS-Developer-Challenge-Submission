51f6b16df4075dce376c3997b4e6ecf253a96901
Alex Davis <xandaron@gmail.com>
2025-04-10 00:33:22 +0100

    main.go

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"HMCTS-Developer-Challenge/api"
 	"HMCTS-Developer-Challenge/database"
-	// "HMCTS-Developer-Challenge/api"
+	"HMCTS-Developer-Challenge/session"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,23 +21,27 @@ func main() {
 	loadTemplates()
 
 	if err := db.Connect(); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 
-	http.HandleFunc("/", servePage(templates[HomePage]))
+	http.HandleFunc("/", serveHomePage)
+
 	http.HandleFunc("/login", servePage(templates[LoginPage]))
+	http.HandleFunc("/api/login", apiWrapper(api.HandleLogin))
+
 	http.HandleFunc("/signup", servePage(templates[SignUpPage]))
+	http.HandleFunc("/api/signup", apiWrapper(api.HandleSignUp))
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Switch to HTTPS in production
 	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	if err := db.Disconnect(); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 }
 
@@ -48,8 +53,45 @@ func loadTemplates() {
 	templates[SignUpPage] = template.Must(template.ParseFiles(baseTemplate, navbarTemplate, "./templates/signup.html"))
 }
 
+func serveHomePage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	type Data struct {
+		Name string
+	}
+
+	var data *Data
+	if userID, err := session.GetUserIDFromSession(w, r); err == nil {
+		if name, err := db.GetUserName(userID); err == nil {
+			data = &Data{
+				Name: name,
+			}
+		} else {
+			data = &Data{
+				Name: "Guest",
+			}
+		}
+	} else {
+		data = &Data{
+			Name: "Guest",
+		}
+	}
+
+	if err := templates[HomePage].Execute(w, data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func servePage(template *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
 		if err := template.Execute(w, nil); err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
